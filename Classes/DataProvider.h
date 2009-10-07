ab2de6d22c0fab1e59212ad3beda2c23427e5e1f
Adrian Kosmaczewski <akosma@gmail.com>
2009-10-07 21:17:01 +0200

    Added a DataProvider class

---
+++ Classes/DataProvider.h
@@ -0,0 +1,16 @@
+//
+//  DataProvider.h
+//  Backlog
+//
+//  Created by Adrian on 10/7/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DataProvider : NSObject {
+
+}
+
+@end
---
+++ Classes/DataProvider.m
@@ -0,0 +1,14 @@
+//
+//  DataProvider.m
+//  Backlog
+//
+//  Created by Adrian on 10/7/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "DataProvider.h"
+
+
+@implementation DataProvider
+
+@end
